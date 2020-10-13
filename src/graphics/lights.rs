57b490a02078fa486bbc30f842bd4b68556fc621
ruoshui-git <ruoshuim@gmail.com>
2020-10-13 19:19:32 -0400

    fix/lights.rs: fix bright spots

--- src/graphics/lights.rs
+++ src/graphics/lights.rs
@@ -1,16 +1,20 @@
 use crate::graphics::{vector::Vec3, RGB};
-///
-/// view: view vector
-/// ambient: color of ambient light
-/// light: (position from )
+/// Represents lighting configuration
 #[derive(Copy, Clone, Debug)]
 pub struct LightConfig {
+    /// view: view vector
     pub view: Vec3,
+    /// ambient: color of ambient light
     pub ambient_color: RGB,
+    /// color of directional light source
     pub dir_color: RGB,
+    /// location of directional light source
     pub dir_vec: Vec3,
+    /// ambient reflection const
     pub areflect: Vec3,
+    /// diffuse reflection const
     pub dreflect: Vec3,
+    /// specular reflection const
     pub sreflect: Vec3,
 }
 
@@ -20,15 +24,13 @@ impl LightConfig {
         let viewn = self.view.norm();
         let dirvecn = self.dir_vec.norm();
 
-        let ndotdir: f64 = normaln * dirvecn;
+        let ndotdir: f64 = normaln.dot(dirvecn).max(0.);
 
         let iambient: Vec3 = self.areflect.mul_across(Vec3::from(self.ambient_color));
         let idiffuse: Vec3 = Vec3::from(self.dir_color).mul_across(self.dreflect) * ndotdir;
         let ispecular: Vec3 = Vec3::from(self.dir_color).mul_across(self.sreflect)
-            * (((2 * normaln * ndotdir - dirvecn) * viewn).powi(10));
+            * (((2 * normaln * ndotdir - dirvecn) * viewn).max(0.).powi(10));
 
-        // dbg!(iambient, idiffuse, ispecular);
-        // dbg!(iambient.limit(255.), idiffuse.limit(255.), ispecular.limit(255.));
         (iambient.limit(0., 255.) + idiffuse.limit(0., 255.) + ispecular.limit(0., 255.)).into()
     }
 
@@ -41,24 +43,12 @@ impl LightConfig {
         },
         dir_color: RGB {
             red: 0,
-            green: 255,
+            green: 0,
             blue: 255,
         },
-        dir_vec: Vec3(0.5, 0.75, -1.),
+        dir_vec: Vec3(0.5, 0.75, 1.),
         areflect: Vec3(0.1, 0.1, 0.1),
         dreflect: Vec3(0.5, 0.5, 0.5),
         sreflect: Vec3(0.5, 0.5, 0.5),
     };
 }
-
-// pub(crate) fn test_light() -> LightConfig {
-//     LightConfig {
-//         view: Vec3(0., 0., 1.),
-//         ambient_color: RGB::new(50, 50, 50),
-//         dir_color: RGB::new(0, 255, 255),
-//         dir_vec: Vec3(0.5, 0.75, -1.),
-//         areflect: Vec3(0.1, 0.1, 0.1),
-//         dreflect: Vec3(0.5, 0.5, 0.5),
-//         sreflect: Vec3(0.5, 0.5, 0.5),
-//     }
-// }
