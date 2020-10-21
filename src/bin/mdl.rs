ca5609ff4d7d7b11e4b16addb4bb30af4a8499d3
ruoshui-git <ruoshuim@gmail.com>
2020-10-21 11:00:11 -0400

    refactor/bin: rename mdl to Interpreter

--- src/bin/mdl.rs
+++ src/bin/mdl.rs
@@ -1,6 +1,6 @@
 use std::{env, process};
 
-use graphics::mdl::Interpreter as mdl;
+use graphics::mdl::Interpreter;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,7 +9,7 @@ fn main() {
         process::exit(1);
     }
 
-    mdl::new(&args[1]).run().unwrap_or_else(|e| {
+    Interpreter::new(&args[1]).run().unwrap_or_else(|e| {
         eprintln!("engine error: {}", e);
         process::exit(1);
     });
